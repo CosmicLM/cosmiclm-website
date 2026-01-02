d3390b5d3983c607f30f3e5a7d1fcb0f0050f7ec
CosmicLM <edsldev25@gmail.com>
2026-01-02 00:26:33 -0700

    chore: Initializing Typescript transfer
    This commit introduces documentation for the git commit message file structure and format used in the cosmiclm-website project.

--- main.js
+++
@@ -1,7 +0,0 @@
-import { BlochVisualizer } from "./src/shaders/blochsphere/BlochVisualizer";
-
-const canvas = document.getElementById('bloch-sphere');
-const visualizer = new BlochVisualizer(canvas); 
-visualizer.initializeScene(); 
-visualizer.addSphere();
-visualizer.update();
